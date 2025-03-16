0c4720d13e0d00f29f5a47cb4d2e40954bc3f8b4
Olivier <olivier.huguenot@gmail.com>
2025-03-16 15:10:44 +0100

    dual latch relays ok

--- include/common.h
+++ include/common.h
@@ -5,3 +5,4 @@
 #define COMM_RELAY_FXLOOP   8
 
 #define COMM_HEADER         0xcc
+#define COMM_RESET          0xdd
--- src/amp/main328.cpp
+++ src/amp/main328.cpp
@@ -7,31 +7,63 @@
 // HARDWARE CONFIG
 // add a 4k7 resistor between PIN_SOFTSERIAL and VCC
 // -------------------------------------------------------------
-#define PIN_RELAY_CHANNEL   2
-#define PIN_RELAY_BOOST     4
-#define PIN_RELAY_FXLOOP    6
-#define PIN_SOFTSERIAL      8
+#ifdef DUAL_LATCHING_RELAY
+// no transistor needed
+#define LATCH_PULSE_MS              20
+#define PIN_RELAY_CHANNEL_SET       2
+#define PIN_RELAY_CHANNEL_RESET     3
+#define PIN_RELAY_BOOST_SET         4
+#define PIN_RELAY_BOOST_RESET       5
+#define PIN_RELAY_FXLOOP_SET        6
+#define PIN_RELAY_FXLOOP_RESET      7
+#else
+#define PIN_RELAY_CHANNEL           2
+#define PIN_RELAY_BOOST             4
+#define PIN_RELAY_FXLOOP            6
+#endif
+#define PIN_SOFTSERIAL              8
 
 SoftwareSerial _serial(PIN_SOFTSERIAL, PIN_SOFTSERIAL + 1/* ignore TX */);
 
-void reset() {
-    // set amp default state, all relays off:
-    // - boost ON
-    // - channel DIRT
-    // - fxloop ON
-    digitalWrite(PIN_RELAY_CHANNEL, LOW);
-    digitalWrite(PIN_RELAY_BOOST, LOW);
-    digitalWrite(PIN_RELAY_FXLOOP, LOW);
-}
+typedef enum {
+    RELAY_CHANNEL,
+    RELAY_BOOST,
+    RELAY_FXLOOP,
+} eRelay;
+
 
 void setup() {
     dprintinit(9600);
     dprintln(F("start"));
     _serial.begin(9600);
+#ifdef DUAL_LATCHING_RELAY
+    pinMode(PIN_RELAY_CHANNEL_SET, OUTPUT);
+    pinMode(PIN_RELAY_CHANNEL_RESET, OUTPUT);
+    pinMode(PIN_RELAY_BOOST_SET, OUTPUT);
+    pinMode(PIN_RELAY_BOOST_RESET, OUTPUT);
+    pinMode(PIN_RELAY_FXLOOP_SET, OUTPUT);
+    pinMode(PIN_RELAY_FXLOOP_RESET, OUTPUT);
+
+    // reset to default amp settings
+    digitalWrite(PIN_RELAY_CHANNEL_RESET, HIGH);
+    delay(LATCH_PULSE_MS);
+    digitalWrite(PIN_RELAY_CHANNEL_RESET, LOW);
+
+    digitalWrite(PIN_RELAY_BOOST_RESET, HIGH);
+    delay(LATCH_PULSE_MS);
+    digitalWrite(PIN_RELAY_BOOST_RESET, LOW);
+
+    digitalWrite(PIN_RELAY_FXLOOP_RESET, HIGH);
+    delay(LATCH_PULSE_MS);
+    digitalWrite(PIN_RELAY_FXLOOP_RESET, LOW);
+
+    // TODO: grab reset instructions from remote ?
+
+#else
     pinMode(PIN_RELAY_CHANNEL, OUTPUT);
     pinMode(PIN_RELAY_BOOST, OUTPUT);
     pinMode(PIN_RELAY_FXLOOP, OUTPUT);
-    reset();
+#endif
 }
 
 void loop() {
@@ -50,21 +82,34 @@ void loop() {
             dprint(F(" - state: "));
             dprintln(state);
             if (relay == COMM_RELAY_CHANNEL) {
+                dprint(F("CHANNEL "));
+                dprintln(state ? F("DIRTY") : F("CLEAN"));
+#ifdef DUAL_LATCHING_RELAY
+                digitalWrite(state ? PIN_RELAY_CHANNEL_SET : PIN_RELAY_CHANNEL_RESET, HIGH);
+                delay(LATCH_PULSE_MS);
+                digitalWrite(state ? PIN_RELAY_CHANNEL_SET : PIN_RELAY_CHANNEL_RESET, LOW);
+#else
                 digitalWrite(PIN_RELAY_CHANNEL, state);
+#endif
             }
             else if (relay == COMM_RELAY_BOOST) { 
-                digitalWrite(PIN_RELAY_BOOST, state);
+                dprint(F("BOOST "));
+                dprintln(state ? F("ON") : F("OFF"));
+                digitalWrite(state ? PIN_RELAY_BOOST_SET : PIN_RELAY_BOOST_RESET, HIGH);
+                delay(LATCH_PULSE_MS);
+                digitalWrite(state ? PIN_RELAY_BOOST_SET : PIN_RELAY_BOOST_RESET, LOW);
             }
             else if (relay == COMM_RELAY_FXLOOP) {
-                digitalWrite(PIN_RELAY_FXLOOP, state);
+                dprint(F("FXLOOP "));
+                dprintln(state ? F("ON") : F("OFF"));
+                digitalWrite(state ? PIN_RELAY_FXLOOP_SET : PIN_RELAY_FXLOOP_RESET, HIGH);
+                delay(LATCH_PULSE_MS);
+                digitalWrite(state ? PIN_RELAY_FXLOOP_SET : PIN_RELAY_FXLOOP_RESET, LOW);
             }
         } 
         else {
             dprintln(F("wrong header"));
         }
-    }
-    else {
-        dprint(".");
     }
     delay(20);
 }
